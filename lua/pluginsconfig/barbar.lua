757f6f4744e5f8d68a2457bfb499b1e85735b9b2
Renan Brayner <renanbrayner@gmail.com>
2022-03-31 03:41:22 -0300

    remove barbar separators

--- lua/pluginsconfig/barbar.lua
+++ lua/pluginsconfig/barbar.lua
@@ -32,8 +32,10 @@ vim.g.bufferline = {
   icon_custom_colors = false,
 
   -- Configure icons on the bufferline.
-  icon_separator_active = '▎',
-  icon_separator_inactive = '▎',
+  -- icon_separator_active = '▎',
+  icon_separator_active = '',
+  -- icon_separator_inactive = '▎',
+  icon_separator_inactive = '',
   icon_close_tab = '×',
   -- icon_close_tab_modified = '',
   icon_close_tab_modified = '⏺',
