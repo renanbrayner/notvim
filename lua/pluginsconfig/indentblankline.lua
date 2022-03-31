578ead8f235bc179da6d140f4fc6dd3b55d4b65d
Renan Brayner <renanbrayner@gmail.com>
2022-03-30 22:55:47 -0300

    change trail character

--- lua/pluginsconfig/indentblankline.lua
+++ lua/pluginsconfig/indentblankline.lua
@@ -12,7 +12,7 @@ vim.g.indent_blankline_filetype_exclude = {
 vim.opt.list = true
 vim.opt.listchars:append("space:·")
 -- vim.opt.listchars:append("eol:﬋")
-vim.opt.listchars:append("trail:")
+vim.opt.listchars:append("trail:")
 
 require("indent_blankline").setup {
     space_char_blankline = " ",
