ff1ba93c42d90f8725d799fa95c3719fafad2ead
Renan Brayner <renanbrayner@gmail.com>
2022-07-09 19:55:09 -0300

    fix some stuff

--- lua/pluginsconfig/treesitter.lua
+++ lua/pluginsconfig/treesitter.lua
@@ -4,7 +4,7 @@ if not status_ok then
 end
 
 configs.setup {
-  ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
   ignore_install = { "" }, -- List of parsers to ignore installing
   autopairs = {
@@ -12,7 +12,7 @@ configs.setup {
   },
   highlight = {
     enable = true, -- false will disable the whole extension
-    disable = { "sass", "scss", "css", "yaml" }, -- list of language that will be disabled, sass, scss and css had errors with me
+    disable = { "sass", "scss", "css", "yaml", "svelte" }, -- list of language that will be disabled, sass, scss and css had errors with me
     additional_vim_regex_highlighting = true,
   },
   indent = { enable = true, disable = { "yaml" } },
@@ -22,5 +22,5 @@ configs.setup {
   },
 }
 
-vim.opt.foldmethod = "expr"
-vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+-- vim.opt.foldmethod = "expr"
+-- vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
