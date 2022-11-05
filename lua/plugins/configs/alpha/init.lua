17ceb186821d335f0d6cef2be103ddd2b1f2441b
Renan Brayner <renanbrayner@gmail.com>
2022-11-05 15:35:43 -0300

    Config new plugins

---
+++ lua/plugins/configs/alpha/init.lua
@@ -0,0 +1,33 @@
+local alpha = require("alpha")
+local headers = require("plugins.configs.alpha.headers")
+local dashboard = require("alpha.themes.dashboard")
+
+dashboard.section.header.val = headers.sleekraken
+
+dashboard.section.buttons.val = {
+  dashboard.button("f", "  Find file", ":call ControlP()<CR>"),
+  dashboard.button("r", "  Recent", ":History<CR>"),
+  dashboard.button("l", "  Load Last session", ":SessionManager load_last_session<CR>"),
+  dashboard.button("s", "  Load session", ":SessionManager load_session<CR>"),
+  -- dashboard.button( "m", "  Bookmarks", ":Marks <CR>"),
+  dashboard.button("c", "  Configuration", ":e $MYVIMRC<CR>:cd %:p:h<CR>:pwd<CR>"),
+  dashboard.button("e", "  New file", ":ene<CR>"),
+  dashboard.button("q", "  Quit NVIM", ":qa<CR>"),
+}
+
+local function footer()
+  local plugins = vim.fn.len(vim.fn.globpath("~/.local/share/nvim/site/pack/packer/start", "*", 0, 1))
+  local v = vim.version()
+  local datetime = os.date(" %d-%m-%Y")
+  return string.format(" %s   v%s.%s.%s  %s", plugins, v.major, v.minor, v.patch, datetime)
+end
+
+dashboard.section.footer.val = footer()
+
+vim.cmd([[
+autocmd FileType alpha setlocal nofoldenable
+autocmd User AlphaReady set showtabline=0 | autocmd BufUnload <buffer> set showtabline=2
+]])
+
+
+alpha.setup(dashboard.opts)
---
+++ lua/plugins/keymaps/telescope.lua
@@ -0,0 +1,8 @@
+local actions = require("telescope.actions")
+local mappings = {
+	i = {
+		["<C-h>"] = actions.which_key,
+	},
+}
+
+return mappings
