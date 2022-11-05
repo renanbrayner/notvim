fea29d39b0cba3825985e6be26b17231b71f6ca6
Renan Brayner <renanbrayner@gmail.com>
2022-11-05 15:35:20 -0300

    Add more configurations

--- lua/opts.lua
+++ lua/opts.lua
@@ -6,5 +6,13 @@ set.clipboard = "unnamedplus" -- share clipboard with system
 set.mouse = "a" -- mouse suport
 set.splitright = true -- non retarded splits
 set.splitbelow = true -- non retarded splits
+set.list = true
+set.listchars:append("eol:﬋")
+set.listchars:append("trail:")
+set.autoindent = true
+set.tabstop = 2
+set.shiftwidth = 2
+set.softtabstop = -1
+set.expandtab = true
 
 vim.g.mapleader = " "
