22ee1976207a1435c63f054238dc17ed517caefd
Renan Brayner <renanbrayner@gmail.com>
2022-11-06 22:26:24 -0300

    add pcall

--- lua/plugins/keymaps/telescope.lua
+++ lua/plugins/keymaps/telescope.lua
@@ -1,8 +1,13 @@
-local actions = require("telescope.actions")
+local actions_ok, actions = pcall(require, "telescope.actions")
+if not actions_ok then
+  vim.notify("Error requiring telescope.actions", error)
+  return
+end
+
 local mappings = {
-	i = {
-		["<C-h>"] = actions.which_key,
-	},
+  i = {
+    ["<C-h>"] = actions.which_key,
+  },
 }
 
 return mappings
