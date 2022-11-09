e34241b9edcf03cbfb29f9a9405d256d013f36e8
Renan Brayner <renanbrayner@gmail.com>
2022-11-09 07:27:35 -0300

    documentation

--- init.lua
+++ init.lua
@@ -1,3 +1,20 @@
+--  nvim ←[ root folder ]-
+-- ├──  lua
+-- │  ├──  lsp
+-- │  │  ├──  init.lua ←[ lsp setup ]-
+-- │  │  ├──  handlers.lua ←[ on_atacch and capabilities etc ]-
+-- │  │  ├──  mason-null-ls.lua ←[ bridge between mason and null-ls ]-
+-- │  │  ├──  mason.lua ←[ lsp installer ]-
+-- │  │  └──  null-ls.lua ←[ formatter linting and etc ]-
+-- │  ├──  plugins
+-- │  │  ├──  configs ←[ plugins configurations ]-
+-- │  │  ├──  keymaps ←[ plugins keymaps and whichkey ]-
+-- │  │  └──  init.lua ←[ packer file ]-
+-- │  ├──  opts.lua ←[ vim options ]-
+-- │  ├──  rice.lua ←[ aesthetics stuff ]-
+-- │  └──  utils.lua ←[ utility functions and etc ]-
+-- └──  init.lua ←[ this file ]-
+
 require 'rice'
 require 'utils'
 require 'opts'
