fcf37168e12af2338aa3ad66b261cbabe5a72cd5
Renan Brayner <renanbrayner@gmail.com>
2022-11-08 21:21:12 -0300

    Fix pears autocomplete

--- lua/plugins/configs/pears.lua
+++ lua/plugins/configs/pears.lua
@@ -6,7 +6,7 @@ end
 
 pears.setup(function(conf)
   conf.on_enter(function(pears_handle)
-    if vim.fn.pumvisible() == 1 and vim.fn.complete_info().selected ~= 0 then
+    if vim.fn.pumvisible() == 1 and vim.fn.complete_info().selected ~= -1 then
       vim.cmd[[:startinsert | call feedkeys("\<c-y>")]]
     else
       pears_handle()
