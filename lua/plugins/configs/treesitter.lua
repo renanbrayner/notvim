de3b9e945171833bab24d93a27a7412850d5f4c0
Renan Brayner <renanbrayner@gmail.com>
2026-08-22 07:52:52 -0300

    treesitter

--- lua/plugins/configs/treesitter.lua
+++ lua/plugins/configs/treesitter.lua
@@ -1,72 +1,65 @@
-local status_ok, treesitter = pcall(require, 'nvim-treesitter.configs')
-if not status_ok then
-  vim.notify('Error requiring nvim-treesitter.configs', vim.log.levels.ERROR)
-end
+local loader = require('utils.loader')
 
-treesitter.setup {
-  ensure_installed = {
-    'javascript',
-    'typescript',
-    'tsx',
-    'vue',
-    'html',
-    'css',
-    'scss',
-    'astro',
-    'svelte',
-    'rust',
-    'python',
-    'c',
-    'cpp',
-    'c_sharp',
-    'go',
-    'java',
-    'kotlin',
-    'dart',
-    'haskell',
-    'ocaml',
-    'elixir',
-    'fsharp',
-    'lua',
-    'bash',
-    'fish',
-    'nu',
-    'json',
-    'toml',
-    'yaml',
-    'dockerfile',
-    'markdown',
-    'xml',
-    'sql',
-    'graphql',
-    'regex',
-    'comment',
-    'gitignore',
-    'gitcommit',
-    'http',
-    'diff',
-  },
-  sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
-  ignore_install = { '' }, -- List of parsers to ignore installing
-  autotag = {
-    enable = true,
-  },
-  autopairs = {
-    enable = true,
-  },
-  highlight = {
-    enable = true, -- false will disable the whole extension
-    disable = { 'sass', 'scss', 'css', 'yaml' }, -- list of language that will be disabled, sass, scss and css had errors with me
-    additional_vim_regex_highlighting = true,
-  },
-  indent = { enable = true, disable = { 'yaml' } },
-  context_commentstring = {
-    enable = true,
-    enable_autocmd = false,
-  },
-  rainbow = {
-    enable = false,
-    extended_mode = true, -- Highlight also non-parentheses delimiters, boolean or table: lang -> boolean
-    max_file_lines = 1000, -- Do not enable for files with more than 1000 lines, int
-  },
+local languages = {
+  'javascript',
+  'typescript',
+  'tsx',
+  'vue',
+  'html',
+  'css',
+  'scss',
+  'astro',
+  'svelte',
+  'rust',
+  'python',
+  'c',
+  'cpp',
+  'c_sharp',
+  'go',
+  'java',
+  'kotlin',
+  'dart',
+  'haskell',
+  'ocaml',
+  'elixir',
+  'fsharp',
+  'lua',
+  'bash',
+  'fish',
+  'nu',
+  'json',
+  'toml',
+  'yaml',
+  'dockerfile',
+  'markdown',
+  'xml',
+  'sql',
+  'graphql',
+  'regex',
+  'comment',
+  'gitignore',
+  'gitcommit',
+  'http',
+  'diff',
 }
+
+-- Setup do nvim-treesitter (nova API minimalista — branch main)
+loader.safe_setup('nvim-treesitter', {
+  install_dir = vim.fn.stdpath('data') .. '/site',
+})
+
+-- Instala parsers em background na primeira inicialização / em :TSUpdate
+vim.api.nvim_create_autocmd('User', {
+  pattern = 'TSUpdate',
+  callback = function()
+    require('nvim-treesitter').install(vim.list_extend({}, languages))
+  end,
+})
+
+-- Ativa syntax highlighting via Neovim nativo (queries vêm do runtimepath do nvim-treesitter)
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = languages,
+  callback = function(args)
+    pcall(vim.treesitter.start, nil, args.buf)
+  end,
+})
